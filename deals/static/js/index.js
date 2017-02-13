0860be44c1f4f5642ae6ab3780efa83b0b8c18e8
Sean Beck <seanmckaybeck@gmail.com>
2017-02-13 15:47:20 +0000

    added percent difference columns

--- deals/static/js/index.js
+++ deals/static/js/index.js
@@ -1,3 +1,11 @@
+function set_color(tag) {
+    if (tag.textContent >= 0)
+        tag.setAttribute('bgcolor', 'crimson');
+    else
+        tag.setAttribute('bgcolor', 'limegreen');
+}
+
+
 function percentage_changed() {
     var percentage = parseInt($('#bucks-percent')[0].value) / 100;
     $('.item').each(function(idx){
@@ -7,17 +15,15 @@ function percentage_changed() {
         if (kids[6].textContent > 100)
             kids[6].textContent = 100;
         kids[7].textContent = (kids[4].textContent - kids[5].textContent - kids[6].textContent).toFixed(2); // melt difference with bucks: price - melt - bucks
-        if (kids[7].textContent >= 0)
-            kids[7].setAttribute('bgcolor', 'crimson');
-        else
-            kids[7].setAttribute('bgcolor', 'limegreen');
-        kids[8].textContent = (kids[4].textContent * 0.02).toFixed(2); // 2% cashback cc: price * 0.02
-        kids[9].textContent = (kids[4].textContent - kids[5].textContent - kids[6].textContent - kids[8].textContent).toFixed(2); // melt difference with bucks and cashback: price - melt - bucks - cashback
-        if (kids[9].textContent >= 0)
-            kids[9].setAttribute('bgcolor', 'crimson');
-        else
-            kids[9].setAttribute('bgcolor', 'limegreen');
-        kids[10].textContent = (kids[6].textContent / kids[3].textContent).toFixed(2); // bucks/ozt: bucks / weight
+        set_color(kids[7]);
+        kids[8].textContent = ((kids[7].textContent / kids[5].textContent) * 100).toFixed(2); // % melt diff = meltdiff / melt * 100
+        set_color(kids[8])
+        kids[9].textContent = (kids[4].textContent * 0.02).toFixed(2); // 2% cashback cc: price * 0.02
+        kids[10].textContent = (kids[4].textContent - kids[5].textContent - kids[6].textContent - kids[9].textContent).toFixed(2); // melt difference with bucks and cashback: price - melt - bucks - cashback
+        set_color(kids[10]);
+        kids[11].textContent = ((kids[10].textContent / kids[5].textContent) * 100).toFixed(2); // % melt cash diff = meltcashdiff / melt * 100
+        set_color(kids[11]);
+        kids[12].textContent = (kids[6].textContent / kids[3].textContent).toFixed(2); // bucks/ozt: bucks / weight
     });
     var table = $('item-table');
     sorttable.makeSortable(table);
