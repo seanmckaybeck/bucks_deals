939ff40c57d671fa1de13c01289edbbdeb2e3775
Sean Beck <seanmckaybeck@gmail.com>
2017-02-16 13:44:36 +0000

    forgot the forms file

--- deals/forms.py
+++ deals/forms.py
@@ -13,7 +13,14 @@ class ItemForm(FlaskForm):
 
 
 class PercentForm(FlaskForm):
-    percentage = SelectField('Bucks percentage', choices=[('1', '1'), ('2', '2'),
-                                                          ('4', '4'), ('6', '6'),
-                                                          ('8', '8'), ('10', '10')], id='bucks-percent')
+    percentage = SelectField('Bucks Percentage',
+                             choices=[(1, 1), (2, 2),
+                                      (4, 4), (6, 6),
+                                      (8, 8), (10, 10)], id='bucks-percent')
+    cashback = SelectField('Credit Card Cashback Percentage',
+                           choices=[(0.5, 0.5), (1, 1),
+                                    (1.5, 1.5), (2, 2),
+                                    (2.5, 2.5), (3, 3),
+                                    (3.5, 3.5), (4, 4),
+                                    (4.5, 4.5), (5, 5)], id='cash-percent', default=2)
 
