2b4b963e0cdb8e9ef7f822437ef464b4cf294e1c
Sean Beck <seanmckaybeck@gmail.com>
2017-02-16 13:42:57 +0000

    Available column now sortable. Cashback amount selectable

--- deals/static/js/index.js
+++ deals/static/js/index.js
@@ -7,24 +7,45 @@ function set_color(tag) {
 
 
 function percentage_changed() {
-    var percentage = parseInt($('#bucks-percent')[0].value) / 100;
+    var bucks = parseInt($('#bucks-percent')[0].value) / 100;
+    var cashback = parseFloat($('#cash-percent')[0].value) / 100;
     $('.item').each(function(idx){
         var kids = $(this).children();
         kids[5].textContent = (kids[3].textContent * prices[kids[2].textContent.toLowerCase()]).toFixed(2); // melt: weight * spot
-        kids[6].textContent = (kids[4].textContent * percentage).toFixed(2); // bucks: price * bucks percentage
+        kids[6].textContent = (kids[4].textContent * bucks).toFixed(2); // bucks: price * bucks percentage
         if (kids[6].textContent > 100)
             kids[6].textContent = 100;
         kids[7].textContent = (kids[4].textContent - kids[5].textContent - kids[6].textContent).toFixed(2); // melt difference with bucks: price - melt - bucks
         set_color(kids[7]);
         kids[8].textContent = ((kids[7].textContent / kids[5].textContent) * 100).toFixed(2); // % melt diff = meltdiff / melt * 100
-        set_color(kids[8])
-        kids[9].textContent = (kids[4].textContent * 0.02).toFixed(2); // 2% cashback cc: price * 0.02
+        set_color(kids[8]);
+        kids[9].textContent = (kids[4].textContent * cashback).toFixed(2); // x% cashback cc: price * cashback percentage
         kids[10].textContent = (kids[4].textContent - kids[5].textContent - kids[6].textContent - kids[9].textContent).toFixed(2); // melt difference with bucks and cashback: price - melt - bucks - cashback
         set_color(kids[10]);
         kids[11].textContent = ((kids[10].textContent / kids[5].textContent) * 100).toFixed(2); // % melt cash diff = meltcashdiff / melt * 100
         set_color(kids[11]);
         kids[12].textContent = (kids[6].textContent / kids[3].textContent).toFixed(2); // bucks/ozt: bucks / weight
     });
-    var table = $('item-table');
-    sorttable.makeSortable(table);
+    $('#item-table').tablesorter();
+    // var table = $('#item-table')[0];
+    // sorttable.makeSortable(table);
 }
+
+/*
+function toggle_columns(checkbox) {
+    if (checkbox.is(':checked')) {
+        $('.percent').show();
+        $('.dollars').hide();
+    }
+    else {
+        $('.percent').hide();
+        $('.dollars').show();
+    }
+}
+
+
+function pageload() {
+    percentage_changed();
+    toggle_columns($($('#toggle')[0]));
+}
+*/
